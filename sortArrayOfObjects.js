e59f51d914c77249197696f84d06641f5b4168b8
Emmanuel Agbavwe <agbavweisaac@gmail.com>
2022-05-08 22:33:45 +0100

    npm init

--- sortArrayOfObjects.js
+++ sortArrayOfObjects.js
@@ -1,53 +1,27 @@
-const arrayOfObjects = [
-    {
-        firstName: 'Ruky',
-        lastName: 'Choji',
-        id: 11,
-        sex: 'male',
-        age: 14,
-        cars: ['ford', 'green', 'blue']
-    },
-    {
-        firstName: 'Caleb',
-        lastName: 'Uzumaki',
-        id: 09.5,
-        sex: 'male',
-        age: 14,
-        cars: ['benz', 'green', 'blue']
-    },
-    {
-        firstName: 'Itua',
-        lastName: 'Minato',
-        id: 5,
-        sex: 'male',
-        sex: 'male',
-        age: 14,
-        cars: ['camry', 'green', 'blue']
-    },
-    {
-        firstName: 'shully',
-        lastName: 'igbavwe',
-        id: 3,
-        sex: 'female',
-        age: 16,
-        nationality: 'Nigerian',
-        cars: ['vox wagen', 'green', 'blue']
-    },
-    {
-        firstName: 'esther',
-        lastName: 'Hinata',
-        id: 9.51,
-        sex: 'female',
-        age: 50,
-        cars: ['tundra', 'green', 'blue']
-    }
-];
+/**
+ *   Description: A helper function that sorts an array of objects.
+ * 
+ *   Copyright 2022 Emmanuel Agbavwe to Present.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 
 /**
  * Sorts an array of objects by a property present one level deep, in any object in the array. If successful, returns the sorted array of objects, else the original array is returned.
- * @param {Array<Object>} arrayOfObjects
- * @param {String} property
- * @returns {Array<Object>} sortedArrayOfObjects
+ * @param {Array<Object>} arrayOfObjects The array of objects to be sorted
+ * @param {String} property The name of the property to be sorted by.
+ * @returns {Array<Object>} A sorted array of objects, if sorting was successful, otherwise returns the original array.
  */
 const sortArrayOfObjects = (arrayOfObjects, property) => {
     if (!arrayOfObjects) return arrayOfObjects;
@@ -61,4 +35,5 @@ const sortArrayOfObjects = (arrayOfObjects, property) => {
             `${el?.[property]}^-.-^${index}`;
     })?.sort(isInt ? (a, b) => Number(a?.split('^-.-^')?.[0]) - Number(b?.split('^-.-^')?.[0]) : undefined)?.map(el => arrayOfObjects[el?.split('^-.-^')?.[1]]);
 };
-console.log(sortArrayOfObjects(arrayOfObjects, 'cars'));
+
+export default sortArrayOfObjects;
